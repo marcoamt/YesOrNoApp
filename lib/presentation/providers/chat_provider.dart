20ff938937ab43e01a6122d5cc47ffe0066b9397
Marco Mendoza <marcoantonio.mendozatafur@nautes.com>
2023-11-11 12:01:47 +0100

    feat: :sparkles: add provider for messages

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messages = [
+    Message(text: "Ciao!", fromWho: FromWho.me),
+    Message(text: "Come va?", fromWho: FromWho.me),
+  ];
+
+  void sendMessage(String text) async {
+    //TODO: implement method
+  }
+}
